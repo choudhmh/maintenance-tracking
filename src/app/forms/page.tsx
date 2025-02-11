9aad417e16e43397a662516025305cd15d6c9e57
choudhmh <mahmud_uk@hotmail.com>
2025-02-11 11:07:37 +0500

    Deleted Form Files

--- src/app/forms/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/forms/page.tsx
-export default function FormsPage() {
-    return (
-      <div>
-        <h1>Forms Page</h1>
-        <p>This is the forms page.</p>
-      </div>
-    );
-  }
